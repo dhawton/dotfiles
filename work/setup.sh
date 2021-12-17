c9a14eb9663c20b00ffea1154a96c6ac4db67369
Daniel Hawton <daniel@hawton.org>
2021-12-17 15:37:06 -0700

    Add installer

---
+++ work/setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function lnfile() {
+  file=$1
+
+  if [[ -f "$HOME/$file" ]]; then
+    echo "Warning: File $HOME/$file exists, will remove"
+    rm -rf $HOME/$file
+  fi
+
+  ln -s $HOME/$file $file
+}
+
+if [[ -d ".local" ]]; then
+  echo "Run from the same directory as the files"
+  exit 1
+fi
+
+if [[ -d "$HOME/.local" ]]; then
+  echo "Warning: Directory $HOME/.local already exists, will try copying instead"
+  cp -R .local/* $HOME/.local/
+else
+  ln -s $HOME/.local .local
+fi
+
+files=(.personalrc .vimrc)
+for file in "${files[@]}"; do
+  echo "Installing $file"
+  lnfile $file
+done
+
+echo "Done"
